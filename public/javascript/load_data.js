3fd253fe13900c8e676ea5529c5c68de5e10f860
JeffErasLindao <jeras64espol.edu.ec>
2023-11-01 09:57:12 -0500

    function added

--- public/javascript/load_data.js
+++ public/javascript/load_data.js
@@ -6,3 +6,14 @@ import {
 } from "./static_data.js";
 
 let fechaActual = () => new Date().toISOString().slice(0, 10);
+
+let cargarPrecipitacion = () => {
+  //Obtenga la función fechaActual
+  //Defina un arreglo temporal vacío
+  //Itere en el arreglo tiempoArr para filtrar los valores de precipitacionArr que sean igual con la fecha actual
+  //Con los valores filtrados, obtenga los valores máximo, promedio y mínimo
+  //Obtenga la referencia a los elementos HTML con id precipitacionMinValue, precipitacionPromValue y precipitacionMaxValue
+  //Actualice los elementos HTML con los valores correspondientes
+};
+
+cargarPrecipitacion();
